cb583345859df30b6700e47e906405304ee70ebd
SK-Yeboah <skyeboah9@gmail.com>
2023-11-16 17:12:24 +0000

    pint

--- main.c
+++ main.c
@@ -77,6 +77,9 @@ int main(int argc, char *argv[])
         else if (strcmp(opcode, "pall") == 0)
         {
             pall(&stack);
+        } else if (strcmp(opcode, "pint") == 0)
+        {
+            pint(&stack, line_number);
         }
         else
         {
